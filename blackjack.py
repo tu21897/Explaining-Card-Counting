1babaef0547f108d89e356c822f03499b63743e8
tunguyen <tu21897@uw.edu>
2022-02-28 14:57:58 -0800

    bug fixes

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,4 @@
+from pickle import GLOBAL
 import random as r
 import time
 import numpy as np
@@ -38,17 +39,21 @@ PAIR_SOL =  np.array([[3,3,3,3,3,3,0,0,0,0],
                     [0,0,0,0,0,0,0,0,0,0],
                     [3,3,3,3,3,3,3,3,3,3]])
 
-WINS = 0
-LOSSES = 0
+WINS = 0.0
+LOSSES = 0.0
+PUSHES = 0.0
+GAMES = 0.0
 
 def main():
-    i = 0
-    while (i < 500000):
+    i = 1
+    while (i <= 1000):
         print("----------------------- Game", i, "-----------------------")
         play_game()
         i += 1
     print("Wins :", WINS)
     print("Losses :", LOSSES)
+    print("Pushes :", PUSHES)
+    print("# Games in terms of bets :", GAMES)
     print("Winrate :", WINS/(WINS+ LOSSES) * 100.0, "%")
 
 def basic_strategy(pv1, pv2, dv): 
@@ -74,39 +79,37 @@ def basic_strategy(pv1, pv2, dv):
     return PAIR_SOL[pv1 - 2][dv - 2]
 
 def play_game():
-    pVals = {0:[(0,[hit()])]}
+    pVals = [Hand(0, 1.0, [hit()])]
     dVals = [hit()]
-    pVals[0][0][1].append(hit())
+    pVals[0].hand.append(hit())
     dVals.append(hit())
-    gameState = Game_State(pVals, dVals, pVals[0].copy())
+    gameState = Game_State(pVals.copy(), dVals, pVals.copy())
     while (not(gameState.is_stack_empty())):
         currHand = gameState.pop_hand()
         decision = -1
-        if (len(currHand[1]) > 2 or len(currHand[1]) == 1):
-            decision = basic_strategy(sum(currHand[1]), 0, gameState.dHand[0])
-        else:
-            decision = basic_strategy(currHand[1][0], currHand[1][1], gameState.dHand[0])
-        while (True):
+        while (not(decision == 0 or decision == 2)):
+            if (len(currHand.hand) > 2 or len(currHand.hand) == 1 or currHand.splitnum > 2):
+                decision = basic_strategy(sum(currHand.hand), 0, gameState.dHand[0])
+            else:
+                decision = basic_strategy(currHand.hand[0], currHand.hand[1], gameState.dHand[0])
             update_game_state(gameState, currHand, decision)
             if (not(decision == 0 or decision == 2)):
                 currHand = gameState.pop_hand()
-            if (decision == 0 or decision == 2):
-                break
-            if (len(currHand[1]) > 2 or len(currHand[1]) == 1):
-                decision = basic_strategy(sum(currHand[1]), 0, gameState.dHand[0])
-            else:
-                decision = basic_strategy(currHand[1][0], currHand[1][1], gameState.dHand[0])
-    while (sum(gameState.dHand) < 17):
+    dVal = sum(gameState.dHand)
+    while (dVal < 17):
         gameState.dHand.append(hit())
-    evaluate_game_state(gameState, sum(gameState.dHand))
+        dVal = sum(gameState.dHand)
+        if (dVal > 21 and 11 in gameState.dHand):
+            gameState.dHand[gameState.dHand.index(11)] = 1
+            dVal = sum(gameState.dHand)
+    evaluate_game_state(gameState, dVal)
 
 def hit():
     global EXCLUDE
     if (len(EXCLUDE) > NUM_NORM_CARDS * SHUFFLE_AFTER * 13 + 1):
         EXCLUDE = []
-    num = r.randint(1, NUM_NORM_CARDS * NUM_DECKS * 13 + 1)
-    EXCLUDE.append(num)
     h = r.choice(list(set([x for x in range(1, NUM_NORM_CARDS * NUM_DECKS * 13 + 1)]) - set(EXCLUDE)))
+    EXCLUDE.append(h)
     if (1 <= h <= NUM_NORM_CARDS * NUM_DECKS):
         return 2
     elif (NUM_NORM_CARDS * NUM_DECKS + 1 <= h <= NUM_NORM_CARDS * NUM_DECKS * 2):
@@ -131,7 +134,7 @@ def hit():
 def to_string(cVal):
     cvs = ''
     fMap = {0:'10', 1:'J', 2:'Q', 3:'K'}
-    if (cVal == 11):
+    if (cVal == 11 or cVal == 1):
         cvs = 'A'
     elif (cVal == 10):
         cvs = fMap[r.randint(0,3)]
@@ -146,50 +149,60 @@ def print_hand(hand):
     return hString
 
 def update_game_state(currGameState, currHand, decision):
+    newCard = hit()
+    if (sum(currHand.hand) + newCard > 21 and newCard == 11):
+        newCard = 1
     if (decision == 1):
-        currHand[1].append(hit())
+        currHand.hand.append(newCard)
         currGameState.push_hand(currHand)
     elif (decision == 2):
-        currHand[1].append(hit())
-        currGameState.push_hand(currHand)
+        currHand.hand.append(newCard)
+        currHand.betSize += currHand.betSize
     elif (decision == 3):
-        sHand1 = (currHand[0] + 1,[currHand[1][0]])
-        sHand2 = (currHand[0] + 1,[currHand[1][1]])
-        if (currHand[0] + 1 in currGameState.pHands):
-            currGameState.pHands.update({currHand[0] + 1: currGameState.pHands[currHand[0] + 1] + [sHand1, sHand2]})
-        else:
-            currGameState.pHands.update({currHand[0] + 1: [sHand1, sHand2]})
+        sHand1 = Hand(currHand.splitnum + 1, currHand.betSize, [currHand.hand[0]])
+        sHand2 = Hand(currHand.splitnum + 1, currHand.betSize, [currHand.hand[1]])
+        currGameState.pHands += [sHand1, sHand2]
+        currGameState.pHands.remove(currHand)
         currGameState.push_hand(sHand1)
         currGameState.push_hand(sHand2)
-        currGameState.pHands[currHand[0]].remove(currHand)
 
 def evaluate_game_state(gameState, dTot):
     global WINS
     global LOSSES
+    global PUSHES
+    global GAMES
+
     i = 1
     dealerHand = ""
     for card in gameState.dHand:
         dealerHand += to_string(card) + "  "
     print("\n    Dealer Hand: ", dealerHand[:len(dealerHand)-2])
-    for k in gameState.pHands:
-        for pHand in gameState.pHands[k]:
-            handTotal = sum(pHand[1])
-            handStr = ""
-            for card in pHand[1]:
-                handStr += to_string(card) + "  "
-            if (handTotal > 21):
-                print("\n    Hand", i,"(", handStr[:len(handStr)-2],"): Bust")
-                LOSSES += 1
-            elif (handTotal > dTot or dTot > 21):
-                print("\n    Hand", i,"(", handStr[:len(handStr)-2],"): Win")
-                WINS += 1
-            elif (handTotal == dTot):
-                print("\n    Hand", i,"(", handStr[:len(handStr)-2],"): Push")
-            else:
-                print("\n    Hand", i,"(", handStr[:len(handStr)-2],"): Loss")
-                LOSSES += 1
-            i += 1
+    for h in gameState.pHands:
+        handTotal = sum(h.hand)
+        handStr = ""
+        for card in h.hand:
+            handStr += to_string(card) + "  "
+        if (handTotal > 21):
+            print("\n    ",h.betSize," Hand", i,"(", handStr[:len(handStr)-2],"): Bust")
+            LOSSES += h.betSize
+            GAMES += h.betSize
+        elif (handTotal > dTot or dTot > 21):
+            print("\n    ",h.betSize," Hand", i,"(", handStr[:len(handStr)-2],"): Win")
+            WINS += h.betSize
+            GAMES += h.betSize
+        elif (handTotal == dTot):
+            print("\n    ",h.betSize," Hand", i,"(", handStr[:len(handStr)-2],"): Push")
+            PUSHES += h.betSize
+            GAMES += h.betSize
+        else:
+            print("\n    ",h.betSize," Hand", i,"(", handStr[:len(handStr)-2],"): Loss")
+            LOSSES += h.betSize
+            GAMES += h.betSize
+        i += 1
 
+# pHands = [hand1, hand2, etc], player hands
+# dHand = [dealer hand], dealer hand
+# hStack = [(splitnum, [hand]), (splitnum2, [hand2]), ...etc], hands with actions left
 class Game_State:
     def __init__(self, pHands, dHand, hStack):
         self.pHands = pHands
@@ -206,9 +219,20 @@ class Game_State:
         return len(self.hStack) == 0
 
     def print(self):
-        print('player hands: ', self.pHands)
+        pHandStr = " " + str([hand.print() for hand in self.pHands]) + ","
+        hStackStr = " " + str([hand.print() for hand in self.hStack]) + ","
+        print('player hands: ', pHandStr[:len(pHandStr)-1])
         print('dealer hand: ', self.dHand)
-        print('hands with actions left: ', self.hStack)
+        print('hands with actions left: ', hStackStr[:len(hStackStr)-1])
+
+class Hand:
+    def __init__(self, splitnum, betSize, hand):
+        self.splitnum = splitnum
+        self.betSize = betSize
+        self.hand = hand
+    
+    def print(self):
+        print('splitnum: ', self.splitnum, 'bet size: ', self.betSize, 'hand: ', self.hand)
 
 if __name__ == "__main__":
     main()
