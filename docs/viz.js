c7e1b511deb5cd5a27e48ea3e43b70f4fde7e2f7
tunguyen <tu21897@uw.edu>
2022-03-13 12:58:22 -0700

    confidence interval broken

--- docs/viz.js
+++ docs/viz.js
@@ -16,6 +16,41 @@
                                 });
     }
 
+    function stdev(start, end, dataCol) {
+        let mean = 0;
+        let variance = 0;
+        let datsum = 0;
+        for (let i = start; i < end; i++) {
+            datsum += dataCol[i];
+        }
+        mean = datsum/(end-1);
+        for (let i = start; i < end; i++) {
+            variance += (dataCol[i] - mean) ^ 2;
+        }
+        return (sqrt(variance), mean);
+    }
+
+    function generateCI(confidence, start, end, dataCol) {
+        let CI;
+        let rCI = [];
+        let std;
+        if (start != 0) {
+            for (let i = 0; i < end - start; i++) {
+                std = stdev(0, start + i, dataCol);
+                CI = std[1] + confidence * (std[0]/sqrt(end - 1))
+                rCI.push((-CI, CI))
+            }
+        } else {
+            rCI.push((0,0))
+            for (let i = start + 1; i < end; i++) {
+                std = stdev(start, end, dataCol);
+                CI = std[1] + confidence * (std[0]/sqrt(end - 1))
+                rCI.push((-CI, CI))
+            }
+        }
+        return rCI
+    }
+
     function vizgen(data) {
         const margin = ({top:10, right:10, bottom:20, left:20});
         const width = 280;
@@ -61,22 +96,31 @@
                 .attr('cy', (data, i) => (height-margin.top) - (i*(yMargin.bandwidth()) + yScale.bandwidth()/2 + margin.top))
                 .style('fill', 'steelblue');
 
-        vizarea.selectAll('.pointLW')
-                .data(data)
-                .enter()
-                .append('circle')
-                .classed('point', true)
-                .attr('r', 4)
-                .attr('cx', data => xScale(data['Lifetime winrate (W/L x(Base))']))
-                .attr('cy', (data, i) => (height-margin.top) - (i*(yMargin.bandwidth()) + yScale.bandwidth()/2 + margin.top))
-                .style('fill', 'red');
+        // vizarea.selectAll('.pointLW')
+        //         .data(data)
+        //         .enter()
+        //         .append('circle')
+        //         .classed('point', true)
+        //         .attr('r', 4)
+        //         .attr('cx', data => xScale(data['Lifetime winrate (W/L x(Base))']))
+        //         .attr('cy', (data, i) => (height-margin.top) - (i*(yMargin.bandwidth()) + yScale.bandwidth()/2 + margin.top))
+        //         .style('fill', 'steelblue');
+        
+        // let arr = [];
+        // for (let row = 1; row <= data.length; row++) {
+        //     arr.push(data[row]['Session ID'])
+        // }
 
-        vizarea.append('pathLW')
+        // console.log(arr);
+        // let confItvl = generateCI(.95, 0, 21, arr);
+        // console.log(confItvl);
+
+        vizarea.append('path')
                 .datum(data)
                 .classed('line', true)
                 .attr('fill', 'none')
-                .attr('stroke', 'red')
-                .attr('stroke-width', 1.5)
+                .attr('stroke', 'tomato')
+                .attr('stroke-width', 2.5)
                 .attr('d', d3.line()
                                 .x(data => {return xScale(data['Lifetime winrate (W/L x(Base))'])})
                                 .y((data, i) => {return (height-margin.top) - (i*(yMargin.bandwidth()) + yScale.bandwidth()/2 + margin.top)})
