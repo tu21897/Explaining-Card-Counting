94f25b7da9b73d025a6af22fbd2baa5eac7fcc4d
tunguyen <tu21897@uw.edu>
2022-03-15 17:35:42 -0700

    labels

--- docs/viz.js
+++ docs/viz.js
@@ -11,9 +11,8 @@
   * Initialize the interative elements once the window is loaded.
   */
     function init() {
-        d3.csv('data/basic_strategy_data.csv').then(function(data) {
-                                    vizgen(data);
-                                });
+
+        d3.csv('data/counting_wong_halves_data.csv').then(function(data) {vizgen(data)});
     }
 
     /**
@@ -28,9 +27,11 @@
     }
 
     async function vizgen(data) {
+        await delay(3);
         const margin = ({top:20, right:5, bottom:46, left:35});
         const width = 280;
-        const height = 700;
+        const height = 600;
+        const INF = new Intl.NumberFormat('en-US');
 
         const vizarea = d3.select('svg')
                             .classed('vizarea', true)
@@ -40,11 +41,10 @@
         const xScale = d3.scaleLinear()
                             .domain([48, 52])
                             .range([margin.left, width - margin.right]);
-
-        let start = 9800;
+        let start = 0;
         let frameSize = 50;
         while (start + frameSize < data.length + 1){
-            await delay(0.002);
+            await delay(0.001);
             vizarea.selectAll("*").remove();
             
             let frame = data.filter(function(d,i){ return i >= start && i < start+frameSize});
@@ -82,7 +82,16 @@
             // Make the axes
             vizarea.append('g')
                     .attr('transform', `translate(0, ${height - margin.bottom})`)
-                    .call(d3.axisBottom(xMargin).ticks(4));
+                    .call(d3.axisBottom(xMargin).ticks(4))
+                    .append('text')
+                        .attr('text-anchor', 'end')
+                        .attr('fill', 'black')
+                        .attr('font-size', '12px')
+                        .attr('font-weight', 'bold')
+                        .attr('x', width - 2 * margin.right)
+                        .attr('y', -10)
+                        .text('Winrate (%)');
+
 
             vizarea.append('g')
                     .attr('transform', `translate(0, ${height - margin.bottom})`)
@@ -91,7 +100,14 @@
 
             vizarea.append('g')
                     .attr('transform', `translate(${margin.left}, 0)`)
-                    .call(d3.axisLeft(yScale));
+                    .call(d3.axisLeft(yScale))
+                    .append('text')
+                        .attr('transform', `translate(20, ${margin.top}) rotate(-90)`)
+                        .attr('text-anchor', 'end')
+                        .attr('fill', 'black')
+                        .attr('font-size', '12px')
+                        .attr('font-weight', 'bold')
+                        .text('Number of Sessions');
 
             // Make the mean lines
             vizarea.append('path')
@@ -128,6 +144,16 @@
                         .attr('cx', frame => xScale(frame['Session winrate (W/L x(Base))']))
                         .attr('cy', (frame, i) => (height-margin.top) - (i*(yMargin.bandwidth()) + yScale.bandwidth()/2 + margin.top))
                         .style('fill', 'steelblue');
+
+            const numGames = vizarea.append('text')
+                                    .attr('text-anchor', 'center')
+                                    .attr('class', 'year')
+                                    .attr('x', 2 * margin.left - 2 * margin.right)
+                                    .attr('y', margin.top - 8)
+                                    .attr('font-weight', 'bold')
+                                    .attr('font-size',15)
+                                    .text(INF.format(1000 * (start+frameSize-1)) + ' Games Simulated');
         }
+        return vizarea.node()
     }
 })();
